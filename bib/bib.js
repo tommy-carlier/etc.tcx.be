9162add197139206d4554b7f71ac626ea28fa126
tommy-carlier <tommy@tcx.be>
2017-11-07 11:12:11 +0100

    Randomly shuffle list of books to read

--- bib/bib.js
+++ bib/bib.js
@@ -51,11 +51,23 @@
     append(dd, 'SPAN', 'Location', boek.vindplaats);
   }
 
+  function shuffle(xs) {
+    var i = xs.length, x, rnd;
+    while(i) {
+      rnd = Math.floor(Math.random() * i);
+      i -= 1;
+      x = xs[i];
+      xs[i] = xs[rnd];
+      xs[rnd] = x;
+    }
+  }
+
   function render(data) {
     if('boeken' in data) {
       var boeken = data.boeken,
           f = d.createDocumentFragment();
 
+      shuffle(boeken);
       for(var n = boeken.length, i = 0; i < n; i++){
         var boek = boeken[i];
         renderBoek(f, boek);
