259a820bf64ca14a01e1cc0ce81708ddd0aecfe6
tommy-carlier <tommy@tcx.be>
2018-01-16 10:34:27 +0100

    Remove unused function

--- bib/bib.js
+++ bib/bib.js
@@ -87,12 +87,6 @@
     list.appendChild(f);
   }
 
-  function compare(a, b) {
-    if(a < b) return -1;
-    if(a > b) return 1;
-    return 0;
-  }
-
   function renderBoeken(boeken) {
     renderItems(lijstTeLezenBoeken, boeken, renderBoek);
   }
